ec306aa1c261328eab60976cacc1380da865e6b9
Alex Gian <alexhgian@gmail.com>
2015-05-25 18:26:54 -0700

    PRoduction

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ server.get('/', function(sreq, sres, snext){
 
 });
 
-
-server.listen(8080, function() {
+var port = process.env.PORT || 8080;
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
