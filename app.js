3c97e60df72d7ff81243d13c702da85cbf9fa3be
Alex Gian <alexhgian@gmail.com>
2015-05-25 18:23:27 -0700

    First

---
+++ app.js
@@ -0,0 +1,49 @@
+var restify = require('restify');
+var assert = require('assert');
+// Creates a JSON client
+var client = restify.createClient({
+    url: 'http://www.monex.com'
+});
+
+
+
+var server = restify.createServer();
+
+//Allow Cross Origin Requests
+server.use(restify.CORS());
+server.use( function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+});
+
+server.get('/', function(sreq, sres, snext){
+    sres.contentType = 'text';
+    client.get('/data/pricefile.dat?_=1432600394468', function(err, req) {
+        assert.ifError(err); // connection error
+
+        req.on('result', function(err, res) {
+            assert.ifError(err); // HTTP status code >= 400
+
+            res.body = '';
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {
+                res.body += chunk;
+            });
+
+            res.on('end', function() {
+                console.log(res.body);
+                sres.send(res.body);
+                snext();
+            });
+        });
+    });
+
+
+
+});
+
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
